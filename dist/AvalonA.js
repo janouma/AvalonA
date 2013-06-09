4b30ef7a58ee61642c2c3a782f4a2dda521cff1e
Judicaël Anouma <judicael.anouma@appitforme.com>
2013-06-09 18:54:14 +0200

    Updated dist with z refresh improvements

--- dist/AvalonA.js
+++ dist/AvalonA.js
@@ -1,50 +1,57 @@
 
-/* AvalonA 0.3.5
+/* AvalonA 0.4.0
 */
 
 
 (function() {
-  var Frame3d;
+  var Frame3d,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   Frame3d = (function() {
-    var deepnessAttribute;
+    var debugName, transitionDuration;
 
-    deepnessAttribute = 'data-avalonA-deepness';
+    transitionDuration = 0.75;
+
+    debugName = function(node) {
+      return "" + (node.prop('tagName')) + "(" + (node.attr('id') || node.attr('class') || node.attr('href')) + ")";
+    };
 
     Frame3d.prototype.find3dFrames = function() {
       this.outerFrameJQueryNode = $("#" + this.id);
-      this.innerFrameJQueryNode = $('.avalona-inner-frame', this.outerFrameJQueryNode).eq(0);
+      this.innerFrameJQueryNode = $("." + this.cssClass, this.outerFrameJQueryNode).eq(0);
+      if (this.debug === true) {
+        console.log("@deepnessAttribute: " + this.deepnessAttribute);
+        console.log("@cssClass: " + this.cssClass);
+      }
       if (!this.outerFrameJQueryNode.size()) {
         throw new Error("Cannot find 3d frame '#" + this.id + "' in dom");
       }
       if (!this.innerFrameJQueryNode.size()) {
-        throw new Error("Cannot find 3d inner frame '#" + this.id + " .avalona-inner-frame' in dom");
+        throw new Error("Cannot find 3d inner frame '#" + this.id + " ." + this.cssClass + "' in dom");
       }
     };
 
     Frame3d.prototype.setUp = function() {
-      TweenLite.set(this.innerFrameJQueryNode[0], {
-        transformPerspective: 1000,
-        transformStyle: 'preserve-3d',
-        overflow: 'visible'
-      });
-      return $("[" + deepnessAttribute + "]", this.innerFrameJQueryNode).each(function() {
-        return TweenLite.set(this, {
-          transformStyle: 'preserve-3d',
-          overflow: 'visible'
-        });
+      return TweenLite.set(this.innerFrameJQueryNode[0], {
+        transformPerspective: 1000
       });
     };
 
     Frame3d.prototype.addBehavior = function() {
-      var _this = this;
+      var debugCode,
+        _this = this;
+      if (this.debug === true) {
+        debugCode = function(rotationX, rotationY) {
+          return console.log("rotationY: " + rotationY);
+        };
+      } else {
+        debugCode = function() {};
+      }
       this.outerFrameJQueryNode.mousemove(function(event) {
         var rotationX, rotationY;
         rotationY = (event.pageX - $(window).prop('innerWidth') / 2) / 25;
         rotationX = -1 * (event.pageY - $(window).prop('innerHeight') / 2) / 15;
-        if (_this.debugIsOn) {
-          console.log("rotationY: " + rotationY);
-        }
+        debugCode(rotationX, rotationY);
         return TweenLite.set(_this.innerFrameJQueryNode[0], {
           rotationX: rotationX,
           rotationY: rotationY
@@ -66,30 +73,69 @@
       return (_ref1 = this.outerFrameJQueryNode) != null ? _ref1.off("mouseout") : void 0;
     };
 
-    Frame3d.prototype.refreshDeepness = function(target) {
-      var targetJqueryNode;
-      if (target == null) {
-        target = null;
+    Frame3d.prototype.zRefresh = function(node) {
+      var firstChild, self, target;
+      if (node == null) {
+        node = null;
       }
-      if (target == null) {
-        target = $("[" + deepnessAttribute + "]", this.innerFrameJQueryNode);
+      self = this;
+      if (node == null) {
+        node = this.innerFrameJQueryNode;
       }
-      targetJqueryNode = typeof target === 'string' ? $(target, this.innerFrameJQueryNode) : $(target);
-      return targetJqueryNode.each(function() {
-        var z;
-        z = $(this).attr(deepnessAttribute);
-        return TweenLite.to(this, .75, {
-          z: z,
-          overflow: 'visible'
-        });
+      target = typeof node === 'string' ? $(target, this.innerFrameJQueryNode) : $(node);
+      if (this.debug === true) {
+        console.log("target: " + (debugName(target)));
+      }
+      this.setZOf(target);
+      firstChild = target.children().eq(0);
+      if (this.debug === true) {
+        console.log("zRefresh firstChild: " + (debugName(firstChild)));
+      }
+      return this.zRefreshChild(firstChild).siblings().each(function() {
+        return self.zRefreshChild($(this));
       });
+    };
+
+    Frame3d.prototype.zRefreshChild = function(child) {
+      if (!child) {
+        throw new Error("zRefreshChild child argument cannot be null");
+      }
+      if ($("[" + this.deepnessAttribute + "]", child).length) {
+        if (this.debug === true) {
+          console.log("zRefresh child " + (debugName(child)) + " has children");
+        }
+        this.zRefresh(child);
+      } else if (child.attr(this.deepnessAttribute)) {
+        if (this.debug === true) {
+          console.log("zRefresh child " + (debugName(child)) + " has '" + this.deepnessAttribute + "'");
+        }
+        this.setZOf(child);
+      }
+      return child;
+    };
+
+    Frame3d.prototype.setZOf = function(target) {
+      var z;
+      if (!target) {
+        throw new Error("setZOf target argument cannot be null");
+      }
+      TweenLite.set(target[0], {
+        transformStyle: 'preserve-3d',
+        overflow: 'visible'
+      });
+      z = target.attr(this.deepnessAttribute);
+      if (z) {
+        return TweenLite.to(target[0], transitionDuration, {
+          z: z
+        });
+      }
     };
 
     Frame3d.prototype.refresh = function() {
       this.removeBehavior();
       this.find3dFrames();
       this.setUp();
-      this.refreshDeepness();
+      this.zRefresh();
       return this.addBehavior();
     };
 
@@ -97,9 +143,15 @@
       return this.refresh();
     };
 
-    function Frame3d(id, debugIsOn) {
+    function Frame3d(id, options) {
       this.id = id;
-      this.debugIsOn = debugIsOn != null ? debugIsOn : false;
+      if (options == null) {
+        options = {};
+      }
+      this.zRefreshChild = __bind(this.zRefreshChild, this);
+      this.debug = options.debug;
+      this.deepnessAttribute = options.zAttr || 'data-avalonA-deepness';
+      this.cssClass = options["class"] || 'avalona-inner-frame';
     }
 
     return Frame3d;
