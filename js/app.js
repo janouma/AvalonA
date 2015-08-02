dd53a9ec337bda9a9ab58e92f312e861d9eef52a
Judicaël Anouma <janouma@gmail.com>
2015-08-02 20:45:58 +0200

    loading hd images only when mediaqueries matches

--- js/app.js
+++ js/app.js
@@ -1,44 +1,72 @@
-[
-	'lightfx',
-	'black-hole'
-].forEach(function loadHdImage(imageBasename){
-	var imageExtension = '.png';
-	var sdImageFile = imageBasename + '-sd' + imageExtension;
-	var hdImagePath = 'images/' + imageBasename + imageExtension;
-	var imageElement = new Image();
+(function startApp() {
 
-	imageElement.addEventListener(
-		'load',
-		function onImageLoad(){
-			var elementList = document.querySelectorAll('img[src$="'+sdImageFile+'"]');
-			Array.prototype.slice.call(elementList)
-				.forEach(function updateSrc(element){
-					element.src = hdImagePath;
-				});
-		},
-		false
-	);
+	/********************
+	* Init steps
+	* *******************/
 
-	imageElement.src = hdImagePath;
-});
+	function loadHdImages() {
+		[
+			'lightfx',
+			'black-hole'
+		].forEach(function loadHdImage(imageBasename) {
+			var imageExtension = '.png';
+			var sdImageFile = imageBasename + '-sd' + imageExtension;
+			var hdImagePath = 'images/' + imageBasename + imageExtension;
+			var imageElement = new Image();
+
+			imageElement.addEventListener(
+				'load',
+				function onImageLoad() {
+					var elementList = document.querySelectorAll('img[src$="' + sdImageFile + '"]');
+					Array.prototype.slice.call(elementList)
+						.forEach(function updateSrc(element) {
+							element.src = hdImagePath;
+						});
+				},
+				false
+				);
+
+			imageElement.src = hdImagePath;
+		});
+	}
 
 
-AvalonA('t3d-frame', 't3d-layer', {
-	tAttr:'transform',
-	fx: function fx(rotation){return rotation * 0.5;},
-	fy: function fy(rotation){return rotation * 0.15;},
+	window.matchMedia = window.matchMedia || window.msMatchMedia;
 
-	animation: AvalonAnimation.Atom({
-		duration: 10,
-		selector: '.gravity',
-		axis: ['z']
-	}),
+	function applyMediaQueries(){
+		if(matchMedia('screen and (min-width: 42em)').matches){
+			loadHdImages();
+		}
+	}
 
-	/*activeArea:{
-		attachment: 'scroll',
-		width: '75%',
-		height: 350
-	},*/
 
-	debug: true
-}).enable();
+	applyMediaQueries();
+
+	window.addEventListener('resize', applyMediaQueries, false);
+
+
+	/********************
+	* Start Avalon (A)
+	* *******************/
+
+	AvalonA('t3d-frame', 't3d-layer', {
+		tAttr: 'transform',
+		fx: function fx(rotation) { return rotation * 0.5; },
+		fy: function fy(rotation) { return rotation * 0.15; },
+
+		animation: AvalonAnimation.Atom({
+			duration: 10,
+			selector: '.gravity',
+			axis: ['z']
+		}),
+
+		/*activeArea:{
+			attachment: 'scroll',
+			width: '75%',
+			height: 350
+		},*/
+
+		debug: true
+	}).enable();
+
+})();
