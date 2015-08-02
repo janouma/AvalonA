3c8730f5a46b9f66e9f915912518fd4d97cd79c7
Judicaël Anouma <janouma@gmail.com>
2015-08-02 18:40:34 +0200

    updating images with hd versions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+[
+	'lightfx',
+	'black-hole'
+].forEach(function loadHdImage(imageBasename){
+	var imageExtension = '.png';
+	var sdImageFile = imageBasename + '-sd' + imageExtension;
+	var hdImagePath = 'images/' + imageBasename + imageExtension;
+	var imageElement = new Image();
+
+	imageElement.addEventListener(
+		'load',
+		function onImageLoad(){
+			var elementList = document.querySelectorAll('img[src$="'+sdImageFile+'"]');
+			Array.prototype.slice.call(elementList)
+				.forEach(function updateSrc(element){
+					element.src = hdImagePath;
+				});
+		},
+		false
+	);
+
+	imageElement.src = hdImagePath;
+});
+
+
 AvalonA('t3d-frame', 't3d-layer', {
 	tAttr:'transform',
 	fx: function fx(rotation){return rotation * 0.5;},
